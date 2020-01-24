6d1c265ec14dddf9c83a35291fa4e46a82437b0c
OverLeo007 <57536503+OverLeo007@users.noreply.github.com>
2020-01-24 18:52:24 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import os
-pygame.mixer.init(channels=2)
-print(os.getcwd() + '\\data\\boom_music.mp3')
-boom = pygame.mixer.Sound(os.path.join('data', 'boom_sound.wav'))
-boom.play(loops=-1)
-while True:
-    pass
